dfa3dbf830f712f9f57355db8a6e0bc505d5b7eb
DragoniteCarnoage <82217818+DragoniteCarnoage@users.noreply.github.com>
2021-04-09 18:16:40 +0530

    QuiclyQr
    V 1.0

---
+++ template/header.php
@@ -0,0 +1,12 @@
+<div class="bg-primary position-relative">
+  <div class="overlay-gradient"></div>
+  <div class="container position-relative">
+  	<div class="row py-5">
+    	<div class="col">
+        <h1 class="display-1">QuickyQr Generator</h1>
+        <p>All in one Stop.<br>Make life simple <code>/template/<span class="bg-danger text-white px-1">header.php</span></code>.</p>
+        <p><a href="#" class="btn btn-primary btn-lg shadow" role="button">Learn more &raquo;</a></p>
+    </div>
+    </div>
+  </div>
+</div>
